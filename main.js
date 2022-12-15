dd051c0742c8b05ba9defdc2a68fece0e2f74262
ChickenHub <84151164+ElectricplaysGaming@users.noreply.github.com>
2022-12-15 08:45:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var iframe = document.getElementById('MyIFrame');
-iframe.contentDocument.body.addEventListener('mouseup', Handler);
+iframe.contentDocument.body.addEventListener('keydown', Handler);
 
 function Handler() {
     alert('works');
